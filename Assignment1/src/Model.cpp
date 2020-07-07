6aec21b549c9dde48060d752be4d11a380e1480b
Dan <dan.raileanu@gmail.com>
2020-07-07 17:54:49 -0400

    minor change to create1()

--- Assignment1/src/Model.cpp
+++ Assignment1/src/Model.cpp
@@ -28,13 +28,13 @@ void Model::createN() {
 	glm::mat4 transform;
 
 	node = new SceneNode(cube);
-	transform = glm::mat4(
+	transform = glm::scale(glm::mat4(1.0f), glm::vec3(2.0f, 2.0, 1.0f));
+	transform = transform * glm::mat4(
 		glm::vec4(1.0f, -1.5f, 0.0f, 0.0f),
 		glm::vec4(0.0f, 1.0f, 0.0f, 0.0f),
 		glm::vec4(0.0f, 0.0f, 1.0f, 0.0f),
 		glm::vec4(0.0f, 0.0f, 0.0f, 1.0f)
 		);
-	transform = glm::scale(transform, glm::vec3(2.0f, 2.0, 1.0f));
 	node->setTransform(transform);
 	addChild(node);
 
@@ -62,14 +62,14 @@ void Model::create1() {
 	addChild(node);
 
 	node = new SceneNode(cube);
-	transform = glm::translate(glm::mat4(1.0f), glm::vec3(-1.125f, 1.375f, 0.0f));
+	transform = glm::translate(glm::mat4(1.0f), glm::vec3(-1.125, 1.5f, 0.0f));
+	transform = glm::scale(transform, glm::vec3(1.25f, 1.0f, 1.0f));
 	transform = transform * glm::mat4(
 		glm::vec4(1.0f, 1.0f, 0.0f, 0.0f),
 		glm::vec4(0.0f, 1.0f, 0.0f, 0.0f),
 		glm::vec4(0.0f, 0.0f, 1.0f, 0.0f),
 		glm::vec4(0.0f, 0.0f, 0.0f, 1.0f)
 		);
-	transform = glm::scale(transform, glm::vec3(1.25f, 1.0f, 1.0f));
 	node->setTransform(transform);
 	addChild(node);
 }
